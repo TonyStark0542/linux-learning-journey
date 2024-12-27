0e4d64b33d9b5a0735a0b5e3135a9afeaea9d712
Mahesh H <105983460+TonyStark0542@users.noreply.github.com>
2024-12-27 20:25:38 +0530

    Create 02_basic_commands.sh

---
+++ basic-commands/02_basic_commands.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Basic Linux commands
+
+# Listing files
+echo "Listing files:"
+ls
+
+# Navigating directories
+echo "Navigating to home directory:"
+cd ~
+
+# Creating and removing directories
+mkdir test_directory
+echo "Created a new directory called test_directory."
+
+rmdir test_directory
+echo "Removed the directory test_directory."
