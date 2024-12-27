6040da1036003636f9d07ffcbad19cc3bfe486c9
Mahesh H <105983460+TonyStark0542@users.noreply.github.com>
2024-12-27 20:23:13 +0530

    Create 05_05_if_else.sh

---
+++ bash_scripting/05_05_if_else.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# This script demonstrates if/elif/else conditionals
+
+echo "Enter a number:"
+read number
+
+if [ $number -gt 10 ]; then
+    echo "The number is greater than 10."
+elif [ $number -eq 10 ]; then
+    echo "The number is equal to 10."
+else
+    echo "The number is less than 10."
+fi
